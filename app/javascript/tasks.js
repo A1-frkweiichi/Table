77540d56140320459f8bad44fe626ebaecf7532b
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-04-22 09:33:55 +0900

    jsテスト

--- app/javascript/tasks.js
+++ app/javascript/tasks.js
@@ -1,20 +1,3 @@
-import { Controller } from "stimulus";
-
-document.addEventListener("DOMContentLoaded", function () {
-  const taskRows = document.querySelectorAll(".task-row");
-
-  taskRows.forEach((row) => {
-    row.addEventListener("click", function () {
-      this.classList.toggle("text-decoration-line-through");
-    });
-  });
-});
-
-import { Controller } from "stimulus";
-
-export default class extends Controller {
-  toggleStrikethrough(event) {
-    const taskText = event.currentTarget.querySelector(".task-text");
-    taskText.classList.toggle("strikethrough");
-  }
+function toggleStrikethrough(event) {
+  event.target.classList.toggle("strikethrough");
 }
